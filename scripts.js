cf1ab93e1378f873e34f8db47e0f6ff2fc05a175
Durgesh <bachchandurgesh@gmail.com>
2024-06-22 20:46:31 +0530

    First commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Currently empty, but you can add interactivity here in the future
+// Example: 
+document.querySelector('.follow').addEventListener('click', () => {
+    alert('Follow button clicked!');
+});
